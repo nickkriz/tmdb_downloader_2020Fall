5ab57350a32b4eb8d59100d6dac81bd6c354d65c
Nicholas B Kriz <nkriz@clemson.edu>
2020-11-08 17:10:47 -0500

    create new folder

--- tmbd_request.py
+++ tmbd_request.py
@@ -1,11 +1,22 @@
 import urllib.request
 import json
 import sys
+import os
 
 api_key = sys.argv[1]
 
-response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
-json_response = json.load(response)
+if not os.patch.exists('json_files'):
+	os.mkdir('json_files')
 
-movie_count = int(json_response['id'])
-print(movie_count)
+# response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
+# json_response = json.load(response)
+
+# movie_count = int(json_response['id'])
+# print(movie_count)
+
+# movie_start = movie_count-5
+
+movie_id = '550'
+response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + api_key)
+json_response = json.load(response)
+print(json_response)
