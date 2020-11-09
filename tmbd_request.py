6df2fafba23027e20300e8a80c6aaeac16047520
Nicholas B Kriz <nkriz@clemson.edu>
2020-11-08 19:58:35 -0500

    request and parse

--- tmbd_request.py
+++ tmbd_request.py
@@ -2,21 +2,29 @@ import urllib.request
 import json
 import sys
 import os
+import time
 
 api_key = sys.argv[1]
 
-if not os.patch.exists('json_files'):
+if not os.path.exists('json_files'):
 	os.mkdir('json_files')
 
-# response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
-# json_response = json.load(response)
-
-# movie_count = int(json_response['id'])
-# print(movie_count)
-
-# movie_start = movie_count-5
-
-movie_id = '550'
-response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + api_key)
+response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
 json_response = json.load(response)
-print(json_response)
+
+movie_count = int(json_response['id'])
+print(movie_count)
+
+movie_start = movie_count-5
+# movie_start = 1
+
+for i in range(movie_start,movie_count):
+	print(i)
+	movie_id = str(i)
+	response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/' + movie_id + '?api_key=' + api_key)
+	json_response = json.load(response)
+	# print(json_response)
+	f = open("json_files/tmdb" + movie_id + ".json", "w")
+	f.write(json.dumps(json_response))
+	f.close()
+	time.sleep(15)
---
+++ tmdb_parse.py
@@ -0,0 +1,47 @@
+import json
+import pandas as pd
+import glob
+import os
+
+if not os.path.exists('parsed_files'):
+	os.mkdir('parsed_files')
+
+df = pd.DataFrame()
+
+for json_file_name in glob.glob('json_files/*.json'):
+	f = open(json_file_name, "r")
+	json_data = json.load(f)
+
+	# print(json_data)
+	df = df.append({
+			'adult' :json_data['adult'],
+			'backdrop_path' :json_data['backdrop_path'],
+			'belongs_to_collection' :json_data['belongs_to_collection'],
+			'budget' :json_data['budget'],
+			'genres' :json_data['genres'],
+			'homepage' :json_data['homepage'],
+			'id' :json_data['id'],
+			'imdb_id' :json_data['imdb_id'],
+			'original_language' :json_data['original_language'],
+			'original_title' :json_data['original_title'],
+			'overview' :json_data['overview'],
+			'popularity' :json_data['popularity'],
+			'poster_path' :json_data['poster_path'],
+			'production_companies' :json_data['production_companies'],
+			'production_countries' :json_data['production_countries'],
+			'release_date' :json_data['release_date'],
+			'revenue' :json_data['revenue'],
+			'runtime' :json_data['runtime'],
+			'spoken_languages' :json_data['spoken_languages'],
+			'status' :json_data['status'],
+			'tagline' :json_data['tagline'],
+			'title' :json_data['title'],
+			'video' :json_data['video'],
+			'vote_average' :json_data['vote_average'],
+			'vote_count' :json_data['vote_count']
+		}, ignore_index=True)
+
+print(df)
+
+df.to_csv('parsed_files/tmdb_dataset.csv')
+
